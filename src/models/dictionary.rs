a5e5a2235c50006c6b0aa36de5f34782715f5921
Chance Linz <chanlinz@amazon.com>
2023-05-24 14:40:59 -0700

    chore: rewriting war and peace

--- src/models/dictionary.rs
+++ src/models/dictionary.rs
@@ -55,7 +55,7 @@ impl Dictionary {
         let all_words = fs::read_to_string("./src/war_and_peace.txt")
             .expect("Could not read book")
             .split(" ")
-            .map(|s| s.trim().to_string())
+            .map(|s| s.trim().to_lowercase().to_string())
             .collect::<Vec<String>>();
 
         let mut map: HashMap<String, i32> = HashMap::new();
